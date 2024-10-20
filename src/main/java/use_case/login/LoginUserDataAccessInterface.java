38a176894bdf45cb0d9cc9f07ef67735cf67b101
Qianwen Wang <qianwen@QianwendeMacBook-Pro.local>
2024-10-20 17:32:02 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set current user.
+     * @param name the user to save
+     */
+    void setCurrentUser(String name);
 }
